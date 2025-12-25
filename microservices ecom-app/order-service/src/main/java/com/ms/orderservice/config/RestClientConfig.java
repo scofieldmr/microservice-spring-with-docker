e49681e908f04ee3745c1d4fe3e399e51bce2447
mdbilal <bilalumar95@gmail.com>
2025-12-25 14:45:00 +0530

    Implementing the distributed tracing using Zipkin

--- microservices ecom-app/order-service/src/main/java/com/ms/orderservice/config/RestClientConfig.java
+++ microservices ecom-app/order-service/src/main/java/com/ms/orderservice/config/RestClientConfig.java
@@ -1,24 +1,33 @@
 package com.ms.orderservice.config;
 
+
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.propagation.Propagator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestClientConfig {
 
-    @Primary
+
     @Bean
-    public RestClient.Builder restClientBuilder() {
-        return RestClient.builder();
+    @Primary
+    public RestClient.Builder restClientBuilder(ObservationRegistry observationRegistry) {
+        return RestClient.builder().observationRegistry(observationRegistry);
     }
+
 
     @LoadBalanced
     @Bean
-    public RestClient.Builder loadbalancedClientBuilder() {
-        return RestClient.builder();
+    public RestClient.Builder loadbalancedClientBuilder(ObservationRegistry observationRegistry) {
+        return RestClient.builder().observationRegistry(observationRegistry);
     }
 
 }
