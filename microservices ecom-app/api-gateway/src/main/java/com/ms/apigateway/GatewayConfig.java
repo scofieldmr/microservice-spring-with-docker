1737ffe9add24353e7bdf7f9bfbc9134734564b9
mdbilal <bilalumar95@gmail.com>
2025-12-26 22:13:57 +0530

    Removing Rewrite path filter in the Java config. in the api-gateway and Using the config for the app

--- microservices ecom-app/api-gateway/src/main/java/com/ms/apigateway/GatewayConfig.java
+++ microservices ecom-app/api-gateway/src/main/java/com/ms/apigateway/GatewayConfig.java
@@ -5,35 +5,35 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 public class GatewayConfig {
 
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("product-service", r -> r
-                        .path("/product/**")
-                        .filters(f -> f.rewritePath(
-                                "/product(?<segment>/?.*)",
-                                "/api/v1/product${segment}"))
+                        .path("/api/v1/product/**")
+//                        .filters(f -> f.rewritePath(
+//                                "/product(?<segment>/?.*)",
+//                                "/api/v1/product${segment}"))
                         .uri("lb://PRODUCT-SERVICE"))
                 .route("user-service",r -> r
-                        .path("/user/**")
-                        .filters(f -> f.rewritePath(
-                                "/user(?<segment>/?.*)",
-                                "/api/v1/user${segment}"))
+                        .path("/api/v1/user/**")
+//                        .filters(f -> f.rewritePath(
+//                                "/user(?<segment>/?.*)",
+//                                "/api/v1/user${segment}"))
                         .uri("lb://USER-SERVICE"))
                 .route("order-service", r -> r
-                        .path("/cart/**")
-                        .filters(f -> f.rewritePath(
-                                "/cart(?<segment>/?.*)",
-                                "/api/v1/cart${segment}"))
+                        .path("/api/v1/cart/**")
+//                        .filters(f -> f.rewritePath(
+//                                "/cart(?<segment>/?.*)",
+//                                "/api/v1/cart${segment}"))
                         .uri("lb://ORDER-SERVICE"))
                 .route("order-service", r -> r
                         .path("/api/v1/order/**")
-                        .filters(f -> f.rewritePath(
-                                "/order(?<segment>/?.*)",
-                                "/api/v1/order${segment}"))
+//                        .filters(f -> f.rewritePath(
+//                                "/order(?<segment>/?.*)",
+//                                "/api/v1/order${segment}"))
                         .uri("lb://ORDER-SERVICE"))
                 .route("eureka-server", r -> r
                         .path("/eureka/main")
